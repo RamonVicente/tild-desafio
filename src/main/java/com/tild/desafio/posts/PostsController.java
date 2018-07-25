2c314bfdce29a137823732df3d384b7cc8e0c204
Ramon Vicente <ramon@localhost.localdomain>
2018-07-25 16:59:15 -0300

    inserção de tags ao post

--- src/main/java/com/tild/desafio/posts/PostsController.java
+++ src/main/java/com/tild/desafio/posts/PostsController.java
@@ -1,6 +1,7 @@
 package com.tild.desafio.posts;
 
 import com.tild.desafio.blog.data.PostRepository;
+import com.tild.desafio.blog.data.TagRepository;
 import com.tild.desafio.blog.data.UserRepository;
 import com.tild.desafio.blog.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +17,20 @@ public class PostsController {
 
     private PostRepository postRepository;
     private UserRepository userRepository;
+    private TagRepository tagRepository; 
 
     @Autowired
-    public PostsController(PostRepository postRepository, UserRepository userRepository) {
+    public PostsController(PostRepository postRepository, UserRepository userRepository, TagRepository tagRepository) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
     }
 
     @GetMapping("/new")
     public ModelAndView newPost(){
         ModelAndView mv = new ModelAndView("new");
-
+        
+        mv.addObject("tags", tagRepository.findAll());
         mv.addObject("users", userRepository.findAll());
         mv.addObject("newPost", new Post());
 
